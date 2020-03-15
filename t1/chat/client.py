604f325f328a6f7273eff0376fdab57b189a3289
ViCesar18 <viniciuscesar_cesar@hotmail.com>
2020-03-15 14:03:03 -0300

    trab1 - chat

---
+++ t1/chat/client.py
@@ -0,0 +1,44 @@
+import socket
+import errno
+import sys
+from threading import Thread
+
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Usuário: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Ajusta o socket para permitir reconexão no mesmo (IP, PORT)
+client_socket.connect((IP, PORT))
+
+username = my_username.encode("utf-8")
+client_socket.send(username)
+
+
+def receberMensagens(): #Recebe as mensagens enviadas por todos os outros clientes conectados ao servidor
+    try:
+        while True:
+                message = client_socket.recv(2048).decode("utf-8")
+
+                print(f"{message}")
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading Error', str(e))
+            sys.exit()
+        return
+
+    except Exception as e:
+        print('Gereral Error', str(e))
+        sys.exit()
+
+
+while True:
+    processo = Thread(target = receberMensagens)    #Processo paralelo que permite o recebimento de mensagens
+    processo.start()                                #por mais que o processo principal esteja aguardando o input
+
+    message = input()   #Envia uma mensagem para o servidor distribuir
+
+    if message:
+        message = message.encode("utf-8")
+        client_socket.send(message)
+
---
+++ t1/chat/server.py
@@ -0,0 +1,58 @@
+import socket
+import select   #Ajuda com I/O de dados entre os diferentes sistemas operacionais
+
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #Cria o socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     #Ajusta o socket para permitir reconexão no mesmo (IP, PORT)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+clients_list = {}
+inputs_list = [server_socket]
+
+while True:
+    #Gerencia a entrada de dados, esperando o canal de entrada notificar que está pronto
+    read_sockets, _, exception_sockets = select.select(inputs_list, [], inputs_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:    #Conecta com o cliente e recebe seus dados (usuário e socket)
+            client_socket, client_adress = server_socket.accept()
+
+            try:
+                user = client_socket.recv(2048)
+            except:
+                user = False
+                
+            if user is False:
+                continue
+            
+            inputs_list.append(client_socket)
+            clients_list[client_socket] = user
+
+            print(f"Nova conexão aceita de {client_adress[0]}:{client_adress[1]}! Usuário:{user.decode('utf-8')}")
+
+        else:   #Recebe a mensagem do cliente
+            user = clients_list[notified_socket]
+            try:
+                message = notified_socket.recv(2048)
+            except:
+                message = False
+
+            if message is False:
+                print(f"Conexão fechada de {user.decode('utf-8')}")
+                inputs_list.remove(notified_socket)
+                del clients_list[notified_socket]
+                continue
+
+            print(f"Mensagem recebida de {user.decode('utf-8')}: {message.decode('utf-8')}")
+
+            for client_socket in clients_list:  #Envia a mensagem recebida para todos os outros clientes conectados no servidor
+                if client_socket != notified_socket:
+                    client_socket.send(user + ": ".encode("utf-8") + message)
+
+    for notified_socket in exception_sockets:   #Caso o select tenha falhado na leitura de algum socket
+        inputs_list.remove(notified_socket)
+        del clients_list[notified_socket]
