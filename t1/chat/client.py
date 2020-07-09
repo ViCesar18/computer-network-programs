97f54ed88e1074dc0d80ff3a488cba12b08692f5
ViCesar18 <viniciuscesar_cesar@hotmail.com>
2020-07-09 18:58:40 -0300

    chat finalizado

--- t1/chat/client.py
+++ t1/chat/client.py
@@ -3,42 +3,43 @@ import errno
 import sys
 from threading import Thread
 
-IP = input("IP: ")
-PORT = int(input("Port: "))
-my_username = input("Usuário: ")
+#IP = "192.168.0.105"
+#PORT = 8888
+#my_username = "vicesar18"
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = input('IP: ')
+PORT = int(input('Porta: '))
+my_username = input('Usuario: ')
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #Cria o socket do cliente
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Ajusta o socket para permitir reconexão no mesmo (IP, PORT)
-client_socket.connect((IP, PORT))
+client_socket.connect((IP, PORT))   #Faz a conexão com o servidor
 
-username = my_username.encode("utf-8")
+username = my_username.encode('utf-8')
 client_socket.send(username)
-
 
 def receberMensagens(): #Recebe as mensagens enviadas por todos os outros clientes conectados ao servidor
     try:
         while True:
-                message = client_socket.recv(2048).decode("utf-8")
-
-                print(f"{message}")
+                message = client_socket.recv(2048).decode('utf-8')
+                print(f'{message}')
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading Error', str(e))
+            print('Erro de Leitura', str(e))
             sys.exit()
         return
 
     except Exception as e:
-        print('Gereral Error', str(e))
+        print('Erro Genérico', str(e))
         sys.exit()
+        
 
+receber = Thread(target=receberMensagens)    #Processo paralelo que permite o recebimento de mensagens
+receber.start()                                #por mais que o processo principal esteja aguardando o input
 
 while True:
-    processo = Thread(target = receberMensagens)    #Processo paralelo que permite o recebimento de mensagens
-    processo.start()                                #por mais que o processo principal esteja aguardando o input
-
     message = input()   #Envia uma mensagem para o servidor distribuir
 
     if message:
-        message = message.encode("utf-8")
+        message = message.encode('utf-8')
         client_socket.send(message)
-
--- t1/chat/server.py
+++ t1/chat/server.py
@@ -1,10 +1,20 @@
 import socket
-import select   #Ajuda com I/O de dados entre os diferentes sistemas operacionais
+import select   #Ajuda com I/O de dados
+from threading import Thread
+
+def fecharServidor():
+    fechar = input('Pressione s para fechar o servidor.\n')
+    while(fechar != 's'):
+        fechar = input()
+
+
+#IP = '192.168.0.105'
+#PORT = 8888
 
 IP = input("IP: ")
-PORT = int(input("Port: "))
+PORT = int(input("Porta: "))
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #Cria o socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #Cria o socket do servidor
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     #Ajusta o socket para permitir reconexão no mesmo (IP, PORT)
 
 server_socket.bind((IP, PORT))
@@ -13,9 +23,19 @@ server_socket.listen()
 clients_list = {}
 inputs_list = [server_socket]
 
+encerrar = Thread(target = fecharServidor)
+encerrar.start()
+
 while True:
     #Gerencia a entrada de dados, esperando o canal de entrada notificar que está pronto
     read_sockets, _, exception_sockets = select.select(inputs_list, [], inputs_list)
+
+    if(not encerrar.is_alive()):
+        for notified_socket in read_sockets:
+            notified_socket.close()
+        for notified_socket in exception_sockets:
+            notified_socket.close()
+        break
 
     for notified_socket in read_sockets:
         if notified_socket == server_socket:    #Conecta com o cliente e recebe seus dados (usuário e socket)
@@ -32,27 +52,31 @@ while True:
             inputs_list.append(client_socket)
             clients_list[client_socket] = user
 
-            print(f"Nova conexão aceita de {client_adress[0]}:{client_adress[1]}! Usuário:{user.decode('utf-8')}")
+            print(f'Nova conexao aceita de {user.decode("utf-8")} ({client_adress[0]}:{client_adress[1]}).')
 
         else:   #Recebe a mensagem do cliente
             user = clients_list[notified_socket]
+            addr = notified_socket.getpeername()
+            addr = f'{addr[0]}:{addr[1]}'
             try:
                 message = notified_socket.recv(2048)
             except:
                 message = False
 
             if message is False:
-                print(f"Conexão fechada de {user.decode('utf-8')}")
+                print(f'Conexao encerrada de {user.decode("utf-8")} ({addr}).')
+                notified_socket.close()
                 inputs_list.remove(notified_socket)
                 del clients_list[notified_socket]
                 continue
 
-            print(f"Mensagem recebida de {user.decode('utf-8')}: {message.decode('utf-8')}")
+            print(f'Mensagem recebida de {user.decode("utf-8")} ({addr}): {message.decode("utf-8")}')
 
             for client_socket in clients_list:  #Envia a mensagem recebida para todos os outros clientes conectados no servidor
                 if client_socket != notified_socket:
-                    client_socket.send(user + ": ".encode("utf-8") + message)
+                    client_socket.send(user + ': '.encode('utf-8') + message + '\n'.encode('utf-8'))
 
     for notified_socket in exception_sockets:   #Caso o select tenha falhado na leitura de algum socket
+        notified_socket.close()
         inputs_list.remove(notified_socket)
         del clients_list[notified_socket]
