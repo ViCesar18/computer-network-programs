0d90b86d8ff9beda5eb8f13587d6704ba9a530c1
Vinicius <vinicius.cesar@uel.br>
2020-09-29 14:28:28 -0300

    finalizando o tratamento udp

--- t6-udp/client.py
+++ t6-udp/client.py
@@ -1,36 +1,66 @@
 import socket
 import os
 
-ip = '192.168.0.107'
+ip = '192.168.0.104'
 port = 8888
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-buffer = 1024
+package_size = 1024
+buffer = []
+buffer_size = 8
 addr = (ip, port)
 
-size = os.stat('./enviado/img.jpg')
+size = os.stat('./enviado/uel.jpg')
 sizeS = size.st_size   #número de pacotes
-Num = int(sizeS / buffer)
+Num = int(sizeS / package_size)
 Num = Num + 1
 print(f"Número de Pacotes para ser enviado: {str(Num)}")
 
 till = str(Num)
-tillC = till.encode('utf-8')
+tillC = till.encode()
 client_socket.sendto(tillC, addr)
 
-tillIC = int(Num);
+tillIC = int(Num)
 
-f = open('./enviado/img.jpg', 'rb')
+f = open('./enviado/uel.jpg', 'rb')
 
 package_number = 0
 
 while tillIC != 0:
-    package = f.read(buffer)
+    package = f.read(package_size)
     client_socket.sendto(package, addr)
+    buffer.append(package)
+
     package_number += 1
     tillIC -= 1
     print(f'Enviando pacote numero: {package_number}')
+
+    if len(buffer) == buffer_size or tillIC == 0:
+        if tillIC == 0:
+            buffer_size = len(buffer)
+        
+        client_socket.sendto(b'QNTD?', addr)
+
+        pacotes_recebidos_pelo_servidor, _ = client_socket.recvfrom(package_size)
+
+        pacotes_recebidos_pelo_servidor = int(pacotes_recebidos_pelo_servidor.decode('utf-8'))
+        i = package_number - buffer_size
+        
+        while pacotes_recebidos_pelo_servidor != buffer_size:
+            client_socket.sendto(b'RES!', addr)
+            for data in buffer:
+                print(f'Reenviando pacote numero: {i}')
+                client_socket.sendto(data, addr)
+                i += 1
+            
+            client_socket.sendto(b'QNTD?', addr)
+            pacotes_recebidos_pelo_servidor = client_socket.recvfrom(package_size)
+            i = package_number - buffer_size
+
+        buffer = []
+        client_socket.sendto(b'OK!', addr)
+        print('porra')
     
 f.close()
 client_socket.close()
--- t6-udp/server.py
+++ t6-udp/server.py
@@ -7,24 +7,41 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((ip, port))
 
 addr = (ip, port)
-buffer = 1024
+package_size = 1024
+buffer_size = 8
+buffer = []
+i = 0
 
-f = open('./recebido/img.jpg', 'wb')
+f = open('./recebido/uel.jpg', 'wb')
 
 package_number = 0
 
-Count, countadress = server_socket.recvfrom(buffer)
+Count, countadress = server_socket.recvfrom(package_size)
 
-tillI = Count.decode('utf-8')
+tillI = Count.decode()
 tillI = int(tillI)
 
-while tillI != 0:
-    data, addr = server_socket.recvfrom(buffer)
-    f.write(data)
-    
-    package_number += 1
-    tillI -= 1
-    print(f"Recebendo pacote numero: {package_number}")
+while True:
+    package, addr = server_socket.recvfrom(package_size)
+
+    if package == b'OK!':
+        print('porra')
+        for data in buffer:
+            f.write(data)
+
+        buffer = []
+    elif package == b'QNTD?':
+        server_socket.sendto(str(i).encode(), addr)
+        i = 0
+    elif package == b'RES!':
+        buffer = []
+    else:
+        buffer.append(package)
+        
+        i += 1
+        package_number += 1
+        tillI -= 1  
+        print(f"Recebendo pacote numero: {package_number}")
 
 f.close()
 server_socket.close()
