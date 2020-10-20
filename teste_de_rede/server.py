10d56e5aae8f0d772f64ec7a3952084ea3b0e17d
Vinicius <vinicius.cesar@uel.br>
2020-10-20 11:15:40 -0300

    melhorando o UDP para o teste de conexão.

--- teste_de_rede/server.py
+++ teste_de_rede/server.py
@@ -9,6 +9,8 @@ server_socket.bind((ip, port))
 
 addr = (ip, port)
 package_size = 1024
+buffer_size = 8
+buffer = []
 i = 0
 
 file_name, client_addr = server_socket.recvfrom(package_size)
@@ -27,16 +29,26 @@ transmission_start = time.time()
 while loop:
     package, client_addr = server_socket.recvfrom(package_size)
 
-    if package == b'END!':
+    if package == b'OK!':
+        for data in buffer:
+            f.write(data)
+        buffer = []
+
+    elif package == b'QNTD?':
+        server_socket.sendto(str(len(buffer)).encode(), client_addr)
+
+    elif package == b'RESET!':
+        buffer = []
+
+    elif package == b'END!':
         loop = False
+
     else:
-        f.write(package)
-
-        server_socket.sendto('RECEBIDO!', client_addr)
-
+        buffer.append(package)
+        
         package_number += 1
         tillI -= 1  
-        print(f"Pacote numero {package_number} recebido!")
+        print(f"Recebendo pacote numero: {package_number}")
 
 f.close()
 server_socket.close()
@@ -48,4 +60,5 @@ print('\nArquivo enviado com sucesso!')
 print(f'Tamanho do arquivo: {package_number * package_size} Bytes')
 print(f'Número de Pacotes transmitidos: {package_number}')
 print(f'Tamanho dos Pacotes: {package_size}')
+print(f'Tamanho do Buffer: {buffer_size}')
 print(f'Velocidade de Transmissão: {round((package_number * package_size * 8) / 1024 / transmission_time, 2)} kb/s') #Multiplicado por 8 para converter Bytes para Bits
